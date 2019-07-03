a95bfbff9a34c0f08fba25bb2116d417c4688885
weaps <weaps@qq.com>
2019-07-03 21:24:27 +0800

    add eslint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
+  data() {
+    return {
+      obj: {
+        name: 'yee',
+        age: 30,
+        sex: 'male',
+        waif: {
+          name: 'zhang',
+          age: 28
+        }
+      }
+    }
+  },
+  mounted() {
+    localStorage.setItem('info', JSON.stringify(this.obj))
+    window.console.log(JSON.parse(localStorage.getItem('info')))
+  },
   components: {
     HelloWorld
   }
